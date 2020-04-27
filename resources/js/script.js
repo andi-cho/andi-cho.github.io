34ef2d581572531612b0750bbd546891c9e28cab
Andi Cho <54452776+andi-cho@users.noreply.github.com>
2020-04-27 15:06:19 -0400

    new changes

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,21 +1,3 @@
-$(document).ready(function() {
-    
-    /* For the main navigation */
-    $('.js--section-features').waypoint(function(direction) {
-        if (direction == "down") {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
-        }
-        
-    }, {
-      offset: '60px;'
-    })
-    
-    /* Animations on link */
-    $('.js--rutgers-link').waypoint(function(direction){
-        $('.js--wp-1').addClass('animated pulse');
-    }, {
-        offset: '50%'
-    })
-}
+$(document).ready(function () {
+
+});
